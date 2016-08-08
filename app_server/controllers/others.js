055942492b8ec75ae25d0eb786f368964851dba0
Tyler2k <thpearso@asu.edu>
2016-08-08 14:08:01 -0700

    added location detail and review views

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,4 @@
-/* GET 'Add review' page */
+/* GET about page */
 module.exports.about = function (req, res) {
-   res.render('index', { title: 'About' });
+   res.render('generic-text', {title: 'About'});
 };
-
